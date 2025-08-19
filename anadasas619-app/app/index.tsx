cfc3a0ac594cfde9b409a9e6e9d8fbe61c37c0cf
Asad Arshad <asadarshad2@hotmail.com>
2025-08-19 14:17:28 +0100

    app test

--- anadasas619-app/app/index.tsx
+++ anadasas619-app/app/index.tsx
@@ -1,16 +1,39 @@
-import { Text, View } from "react-native";
+import { Text, View, StyleSheet } from "react-native";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-      
+    <View style={styles.container}>
+      <Text style={styles.text}>Root Page</Text>
+       <div id="box">
+        hi
+       </div>
     </View>
+   
+
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#f4f4f4ff",
+    borderWidth: 2,
+    borderColor: "red",
+  },
+  text: {
+    fontSize: 20,
+    color: "black",
+  },
+  box: {
+    marginTop: 20,
+    width: 120,
+    height: 120,
+    backgroundColor: "lightblue",
+    borderWidth: 1,
+    borderColor: "blue",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
